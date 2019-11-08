07622c7b66dc78c213c19295ae64a0fb6ed8616c
Cane Rosso <astor@usa.com>
2019-11-08 19:34:27 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -26,6 +26,4 @@ def bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
-  epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
+
